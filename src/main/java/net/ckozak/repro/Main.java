81091cd9a752b7699c22736e044121fcf94dd2e5
Carter Kozak <ckozak@ckozak.net>
2023-12-14 18:02:11 -0500

    Simplify configuration based on feedback from Erik Gahlin
    Added `-Xlog:jfr+system+parser=info,jfr+periodic=debug` logging args
    Removed direct interactions with the `Recording` in favor of
    `RecordingStream`. Our configuration only enables the
    `jdk.JavaThreadStatistics` event.

--- src/main/java/net/ckozak/repro/Main.java
+++ src/main/java/net/ckozak/repro/Main.java
@@ -3,6 +3,7 @@ package net.ckozak.repro;
 import jdk.jfr.Configuration;
 import jdk.jfr.Recording;
 import jdk.jfr.consumer.EventStream;
+import jdk.jfr.consumer.RecordingStream;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -17,27 +18,21 @@ public final class Main {
     }
 
     public static void operation() throws Exception {
-        try (Recording recording = new Recording()) {
-            recording.setToDisk(true);
-            recording.setDumpOnExit(true);
-            recording.setName("my-recording");
-            recording.setSettings(Configuration.getConfiguration("default").getSettings());
-            recording.start();
-            AtomicInteger events = new AtomicInteger();
-            try (EventStream eventStream = EventStream.openRepository()) {
-                eventStream.onEvent("jdk.JavaThreadStatistics", event -> {
-                    System.out.printf("Event: %s%n", event);
-                    events.incrementAndGet();
-                });
-                eventStream.startAsync();
-                long startTime = System.nanoTime();
-                while (events.get() == 0 && System.nanoTime() - startTime < 10_000_000_000L) {
-                    Thread.sleep(100);
-                }
+        AtomicInteger events = new AtomicInteger();
+        try (RecordingStream eventStream = new RecordingStream()) {
+            eventStream.enable("jdk.JavaThreadStatistics").with("period", "1000 ms");
+            eventStream.onEvent("jdk.JavaThreadStatistics", event -> {
+                System.out.printf("Event: %s%n", event);
+                events.incrementAndGet();
+            });
+            eventStream.startAsync();
+            long startTime = System.nanoTime();
+            while (events.get() == 0 && System.nanoTime() - startTime < 10_000_000_000L) {
+                Thread.sleep(100);
             }
-            if (events.get() == 0) {
-                throw new RuntimeException("No events detected");
-            }
+        }
+        if (events.get() == 0) {
+            throw new RuntimeException("No events detected");
         }
     }
 

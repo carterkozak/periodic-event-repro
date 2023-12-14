ca45318ce40efb1971803c07227e27dcd568e7ff
Carter Kozak <ckozak@ckozak.net>
2023-12-14 11:12:01 -0500

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# JDK-21 JFR Periodic Events Missing
+
+The first recording within the process seems to work as expected, however subsequent recordings do not produce periodic events if there's a sufficient pause between recordings.
+
+The resulting JFR recording does not contain periodic events, nor are they picked up by the EventStream or RecordingStream.
+
+If I do not add a sleep between invocations of `Main.operation`, this code
+no longer reproduces the problem. I'm suspicious that periodic event code may get into an error state if it executes without an active recording.
---
+++ src/main/java/net/ckozak/repro/Main.java
@@ -0,0 +1,46 @@
+package net.ckozak.repro;
+
+import jdk.jfr.Configuration;
+import jdk.jfr.Recording;
+import jdk.jfr.consumer.EventStream;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public final class Main {
+
+    public static void main(String[] _args) throws Exception {
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("Iteration %d%n", i);
+            operation();
+            Thread.sleep(1_000);
+        }
+    }
+
+    public static void operation() throws Exception {
+        try (Recording recording = new Recording()) {
+            recording.setToDisk(true);
+            recording.setDumpOnExit(true);
+            recording.setName("my-recording");
+            recording.setSettings(Configuration.getConfiguration("default").getSettings());
+            recording.start();
+            AtomicInteger events = new AtomicInteger();
+            try (EventStream eventStream = EventStream.openRepository()) {
+                eventStream.onEvent("jdk.JavaThreadStatistics", event -> {
+                    System.out.printf("Event: %s%n", event);
+                    events.incrementAndGet();
+                });
+                eventStream.startAsync();
+                long startTime = System.nanoTime();
+                while (events.get() == 0 && System.nanoTime() - startTime < 2_000_000_000L) {
+                    Thread.sleep(100);
+                }
+            }
+            if (events.get() == 0) {
+                throw new RuntimeException("No events detected");
+            }
+        }
+    }
+
+    private Main() {
+    }
+}
